bb87bad1bda948811ae557f8933ab1b95ec53b9c
Cintia Ruiz <cintiaruizv@gmail.com>
2022-10-27 17:04:02 -0500

    sengrid first test

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 import dbConnect from './src/libs/db.js'
 import { server } from './src/server.js'
+import * as dotenv from 'dotenv'
+import * as sgMail from '@sendgrid/mail'
+dotenv.config()
+
+const {
+  SENDGRID_API_KEY
+} = process.env
+
+const handlerMail = new sgMail.MailService()
+
+handlerMail.setApiKey(SENDGRID_API_KEY)
+console.log(SENDGRID_API_KEY)
+
+const sendMail = async (msg) => {
+  try {
+    await handlerMail.send(msg)
+    console.log('msg sent succesfully!')
+  } catch (error) {
+    console.error(error)
+    if (error.response) {
+      console.error(error.response.body)
+    }
+  }
+}
+
+sendMail({
+  to: 'cintiaruizv@gmail.com',
+  from: 'movebikeapp@gmail.com ',
+  subject: 'hola desde movebike',
+  text: 'holaaaaaaaaaa'
+})
 
 dbConnect()
   .then(() => {
